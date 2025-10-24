313b29f9b3e767d63cd56948ee830e560e6fde58
UsUsStudios <biruni056@gmail.com>
2025-10-24 09:27:56 -0600

    main game loop

--- src/main/src/net/dingletherat/torgrays_trials/main/Game.java
+++ src/main/src/net/dingletherat/torgrays_trials/main/Game.java
@@ -1,6 +1,55 @@
 package net.dingletherat.torgrays_trials.main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.time.Duration;
 
 public class Game extends JPanel {
+	public Thread gameThread;
+	public int width = 800;
+	public int height = 600;
+	public double FPS = 0;
+	
+	public void setup() {
+		gameThread = new Thread(this::gameLoop);
+		setPreferredSize(new Dimension(width, height));
+	}
+	
+	public void gameLoop() {
+		final int TARGET_FPS = 60;
+		final long OPTIMAL_TIME = 1_000_000_000 / TARGET_FPS;
+		
+		while (Main.running) {
+			long start = System.nanoTime();
+			
+			update();
+			repaint();
+			
+			long elapsed = System.nanoTime() - start;
+			long sleepTime = OPTIMAL_TIME - elapsed;
+			System.out.println(FPS);
+			
+			if (sleepTime > 0) {
+				try {
+					Thread.sleep(Duration.ofNanos(sleepTime - 750_000));
+					elapsed = System.nanoTime() - start;
+					DecimalFormat df = new DecimalFormat("0.00");
+					FPS = Double.parseDouble(df.format(1_000_000_000.0 / elapsed));
+				} catch (InterruptedException e) {
+					Main.handleException(e);
+				}
+			}
+		}
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, width, height);
+	}
+	
+	private void update() {
+	}
 }
--- src/main/src/net/dingletherat/torgrays_trials/main/Main.java
+++ src/main/src/net/dingletherat/torgrays_trials/main/Main.java
@@ -5,10 +5,24 @@ import javax.swing.*;
 public class Main {
     public static JFrame window;
     public static Game game;
+    public static boolean running = true;
 
     public static void main(String[] args) {
         window = new JFrame();
         window.setTitle("Torgray's Trials");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }   
+        
+        game = new Game();
+        game.setup();
+        window.add(game);
+        window.pack();
+        window.setVisible(true);
+        game.gameThread.start();
+    }
+    
+    public static void handleException(InterruptedException e) {
+        e.printStackTrace();
+        running = false;
+        System.exit(1);
+    }
 }
