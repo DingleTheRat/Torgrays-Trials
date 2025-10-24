c0b7046284942d011eed06779c97434beec250df
Dingle <Stepa202120212021@gmail.com>
2025-10-23 21:05:14 -0600

    New, humble, beginnings

---
+++ src/main/java/net/dingletherat/torgrays_trials/main/Game.java
@@ -0,0 +1,6 @@
+package net.dingletherat.torgrays_trials.main;
+
+import javax.swing.*;
+
+public class Game extends JPanel {
+}
---
+++ src/main/java/net/dingletherat/torgrays_trials/main/Main.java
@@ -0,0 +1,14 @@
+package net.dingletherat.torgrays_trials.main;
+
+import javax.swing.*;
+
+public class Main {
+    public static JFrame window;
+    public static Game game;
+
+    public static void main(String[] args) {
+        window = new JFrame();
+        window.setTitle("Torgray's Trials");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }   
+}
