4953dc2e8173096b7495cbb3686bb67638ca4514
UsUsStudios <biruni056@gmail.com>
2025-10-24 08:27:57 -0600

    added manifest and all of the gradle tasks we need

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "net.dingletherat.torgrays_trials"
+version = "Alpha-2.3"
+
+application {
+    mainClass.set("net.dingletherat.torgrays_trials.main.Main")
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.json:json:20250107") // Json Json 2025
+    implementation("com.github.UsUsStudios:Torgrays-Datagen:v1.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    sourceSets["main"].java.srcDirs("src/main/src")
+}
+
+tasks.register<Jar>("buildFatJar") {
+    archiveBaseName.set("Torgrays-Trials")
+    from(sourceSets["main"].output)
+
+    manifest.from(file("src/main/resources/META-INF/MANIFEST.MF"))
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from({
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
+}
+
+
+tasks.register<JavaExec>("generateData") {
+    mainClass.set("net.dingletherat.torgrays_trials.data_generation.DataGenerator")
+    classpath = sourceSets.main.get().runtimeClasspath
+}
