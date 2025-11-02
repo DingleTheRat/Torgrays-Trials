138516c758359b20c7a0659b632a399a3ceab357
Dingle <Stepa202120212021@gmail.com>
2025-11-02 14:21:39 -0700

    No mice in InputHandler >:(

--- src/main/src/net/dingletherat/torgrays_trials/main/InputHandler.java
+++ src/main/src/net/dingletherat/torgrays_trials/main/InputHandler.java
@@ -7,80 +7,46 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.HashMap;
 
-public class InputHandler {
+public class InputHandler implements KeyListener {
     // Keyboard
-    public static class Keyboard implements KeyListener {
-        public HashMap<Integer, Boolean> keyMap = new HashMap<>(){{
-            // Movement
-            put(KeyEvent.VK_W, false);
-            put(KeyEvent.VK_A, false);
-            put(KeyEvent.VK_S, false);
-            put(KeyEvent.VK_D, false);
-            put(KeyEvent.VK_UP, false);
-            put(KeyEvent.VK_LEFT, false);
-            put(KeyEvent.VK_DOWN, false);
-            put(KeyEvent.VK_RIGHT, false);
-            // Actions
-            put(KeyEvent.VK_ENTER, false);
-            put(KeyEvent.VK_SPACE, false);
-            put(KeyEvent.VK_ESCAPE, false);
-        }};
+    public HashMap<Integer, Boolean> keyMap = new HashMap<>() {{
+        // Movement
+        put(KeyEvent.VK_W, false);
+        put(KeyEvent.VK_A, false);
+        put(KeyEvent.VK_S, false);
+        put(KeyEvent.VK_D, false);
+        put(KeyEvent.VK_UP, false);
+        put(KeyEvent.VK_LEFT, false);
+        put(KeyEvent.VK_DOWN, false);
+        put(KeyEvent.VK_RIGHT, false);
+        // Actions
+        put(KeyEvent.VK_ENTER, false);
+        put(KeyEvent.VK_SPACE, false);
+        put(KeyEvent.VK_ESCAPE, false);
+    }};
 
-        public Keyboard() {
-            Main.LOGGER.info("Loaded keyboard input handler");
-        }
-
-        public void cancelInputs() {
-            keyMap.replaceAll((_, _) -> false);
-        }
-
-        @Override
-        public void keyTyped(KeyEvent e) {
-            System.out.println(e.getKeyChar());
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            int code = e.getKeyCode();
-            keyMap.put(code, true);
-            System.out.println(e);
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            keyMap.put(e.getKeyCode(), false);
-        }
+    public InputHandler() {
+        Main.LOGGER.info("Loaded keyboard input handler");
     }
 
-    // Mouse
-    public static class Mouse implements MouseListener {
-        public Mouse() {
-            Main.LOGGER.info("Loaded mouse input handler");
-        }
+    public void cancelInputs() {
+        keyMap.replaceAll((_, _) -> false);
+    }
 
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            Main.LOGGER.debug("Mouse clicked");
-        }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println(e.getKeyChar());
+    }
 
-        @Override
-        public void mousePressed(MouseEvent e) {
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        keyMap.put(code, true);
+        System.out.println(e);
+    }
 
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-
-        }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keyMap.put(e.getKeyCode(), false);
     }
 }
