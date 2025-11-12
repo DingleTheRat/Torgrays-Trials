6740c8c1323556f81f9e1bbaab9029fc6c7da4a4
Dingle <Stepa202120212021@gmail.com>
2025-11-12 02:46:04 -0700

    Grammar fixes

--- src/main/src/net/dingletherat/torgrays_trials/rendering/UI.java
+++ src/main/src/net/dingletherat/torgrays_trials/rendering/UI.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 import net.dingletherat.torgrays_trials.main.Translations;
 import net.dingletherat.torgrays_trials.main.Game;
@@ -19,12 +20,12 @@ import javax.swing.*;
 
 public class UI {
     /**
-     * The hashMap that holds all UI componenets for each UI state.
+     * The hashMap that holds all UI components for each UI state.
      * Create a UI state by simple making an entry into this hashmap with the key being the name of the uiState.
-     * All the componenets listed in the ArrayList will enable once the UIState variable is equal to the name.
+     * All the components listed in the ArrayList will be enabled once the UIState variable is equal to the name.
      **/
     public HashMap<String, ArrayList<Component>> uiStates = new HashMap<>();
-    /// Depending on what the string is, it will display the corresponding componenets inside the uiStates HashMap.
+    /// Depending on what the string is, it will display the corresponding components inside the uiStates HashMap.
     public String uiState = "Title";
     private String currentUIstate;
 
@@ -61,14 +62,14 @@ public class UI {
     public void update() {
         /*
          * If the currentUIstate isn't equal to the uiState, meaning the uiState has changed, update the components.
-         * This is done to save preformance, as there is no point to update components after the uiState changed.
+         * This is done to save performance, as there is no point in updating components after the uiState changed.
          */
-        if (currentUIstate != uiState) {
-            // Remove each componenet from the old uiState from the game, but only if it exists
+        if (!Objects.equals(currentUIstate, uiState)) {
+            // Remove each component from the old uiState from the game, but only if it exists
             if (uiStates.containsKey(currentUIstate))
                 uiStates.get(currentUIstate).forEach(Main.game::remove);
 
-            // Add each componenet from the new uiState to the game, but also only if it exists
+            // Add each component from the new uiState to the game, but also only if it exists
             if (uiStates.containsKey(uiState))
                 uiStates.get(uiState).forEach(Main.game::add);
             
@@ -78,10 +79,10 @@ public class UI {
     }
 
     public void titleScreen() {
-        // Create componenet pool
+        // Create a component pool
         ArrayList<Component> componentPool = new ArrayList<>();
 
-        // Create a blank space to make the following content be on the middle of the Y axis
+        // Create a blank space to make the following content be in the middle of the Y axis
         componentPool.add(Box.createRigidArea(new Dimension(0, 150)));
 
         // Create the title label, which is bold and slightly red
@@ -90,7 +91,7 @@ public class UI {
         title.setForeground(new Color(209, 25, 25));
         componentPool.add(title);
 
-        // Create a small blank space to seperate the title and buttons
+        // Create a small blank space to separate the title and buttons
         componentPool.add(Box.createRigidArea(new Dimension(0, 20)));
 
         // Create the first button that starts the game when pressed
@@ -101,11 +102,11 @@ public class UI {
         // Create a blank space to not make the buttons stick
         componentPool.add(Box.createRigidArea(new Dimension(0, 5)));
 
-        // A load button (no functionallity for now)
+        // A load button (no functionality for now)
         JLabel loadGame = createBasicText(Translations.translatableText(Main.game.identifier, "load_game"), 80f);
         componentPool.add(loadGame);
 
-        // Another blank space for the same resons as the last one
+        // Another blank space for the same reasons as the last one
         componentPool.add(Box.createRigidArea(new Dimension(0, 5)));
 
         // Finally, a quit button, so players can touch grass
@@ -121,7 +122,7 @@ public class UI {
      * Creates a new {@link JLabel} with plain white text.
      * The text is displayed in the Maru Monica font with the size of your choice.
      * Worth noting that the text label is automatically aligned to the middle,
-     * if you wanna change that call the setAlignmentX or Y methods, and set your alignment.
+     * if you want to change that, call the setAlignmentX or Y methods and set your alignment.
      * <p>
      * @param text The text that will be displayed when the label is shown.
      * @param size The size of the text that will be displayed when the label is shown.
@@ -130,13 +131,15 @@ public class UI {
     public JLabel createBasicText(String text, float size) {
         JLabel label = new JLabel(text);
 
-        // Set the font to Maru Monica, and it's size. As well as make the text color white by making the foreground white.
+        // Set the font to Maru Monica, and it's size. As well as making the text color white by making the foreground white.
         label.setFont(maruMonica.deriveFont(size));
         label.setForeground(Color.WHITE);
 
-        /* Set the alignment (the position of the frame).
+        /*
+        Set the alignment (the position of the frame).
         The benefit of using this over X and Y coordinates is that the frame always sticks to the alignment, no matter the frame's size
-        Also coordinates don't work with layouts, so this is the only option */
+        Also coordinates don't work with layouts, so this is the only option
+         */
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
         label.setAlignmentY(Component.CENTER_ALIGNMENT);
 
@@ -155,7 +158,7 @@ public class UI {
      * @return The label that is created. Feel free to modify it further.
      **/
     public JLabel createBasicButton(String text, float size, Runnable action) {
-        // Create label using createBasicText because it does most of the stuff we need
+        // Create a label using createBasicText because it does most of the stuff we need
         JLabel label = createBasicText(text, size);
 
         // Now for what this method is actually about, hover animations :D
